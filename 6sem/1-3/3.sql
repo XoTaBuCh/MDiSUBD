7dea9ad1b11b0357209a15a47119e8e5f7a8791d
XoTaBuCh <tanzanit2003@gmail.com>
2023-04-11 23:45:46 +0300

    add testing data

--- 6sem/1-3/3.sql
+++ 6sem/1-3/3.sql
@@ -1,168 +1,124 @@
 alter session set current_schema = XOTAB;
 
-CREATE OR REPLACE FUNCTION compare_schemas(dev_schema_name IN VARCHAR2, prod_schema_name IN VARCHAR2)
-    RETURN VARCHAR2
-AS
-    cursor c_dev_tables is
-        SELECT table_name
-        FROM all_tables
-        WHERE owner = dev_schema_name;
-    cursor c_prod_tables is
-        SELECT table_name
-        FROM all_tables
-        WHERE owner = prod_schema_name;
-    cursor c_dev_objects is
-        SELECT object_name, object_type, dbms_metadata.get_ddl(object_type, object_name, dev_schema_name) as ddl
-        FROM all_objects
-        WHERE owner = dev_schema_name
-          AND object_type IN ('FUNCTION', 'PROCEDURE', 'PACKAGE', 'PACKAGE BODY', 'INDEX');
-    cursor c_prod_objects is
-        SELECT object_name, object_type
-        FROM all_objects
-        WHERE owner = prod_schema_name
-          AND object_type IN ('FUNCTION', 'PROCEDURE', 'PACKAGE', 'PACKAGE BODY', 'INDEX');
-    type t_diff_list is table of varchar2(100);
-    diff_list       t_diff_list     := t_diff_list();
-    current_table   varchar2(30);
-    current_object  varchar2(100);
-    first_iteration boolean         := true;
-    result          varchar2(32000) := '';
-BEGIN
-    DBMS_OUTPUT.PUT_LINE('1');
--- Compare tables
-    open c_dev_tables;
-    open c_prod_tables;
-    loop
-        fetch c_dev_tables into current_table;
-        exit when c_dev_tables%notfound;
-        if first_iteration or current_table != dev_row.table_name then
-            if diff_list.count > 0 then
-                result := result || current_table || ' is different in ' || dev_schema_name || ' and ' ||
-                          prod_schema_name || ' schemas' || chr(10);
-                for i in 1..diff_list.count
-                    loop
-                        result := result || diff_list(i) || chr(10);
-                    end loop;
-                diff_list.delete;
-            end if;
-            first_iteration := false;
-        end if;
-
-        if not exists (select 1 from all_tables where owner = prod_schema_name and table_name = current_table) then
-            result := result || current_table || ' is not found in ' || prod_schema_name || ' schema' || chr(10);
-            continue;
-        end if;
-
-        execute immediate 'alter session set current_schema = ' || dev_schema_name;
-        execute immediate 'select dbms_metadata.compare_table_ddl(''' || current_table || ''', ''' || current_table ||
-                          ''', ''' || dev_schema_name || ''', ''' || prod_schema_name || ''') from dual' into result;
-        if result is not null then
-            result := 'Table ' || current_table || ' has differences:' || chr(10) || result || chr(10);
-        end if;
-    end loop;
-
-    if diff_list.count > 0 then
-        result := result || current_table || ' is different in ' || dev_schema_name || ' and ' || prod_schema_name ||
-                  ' schemas' || chr(10);
-        for i in 1..diff_list.count
-            loop
-                result := result || diff_list(i) || chr(10);
-            end loop;
-    end if;
-
-    close c_dev_tables;
-    close c_prod_tables;
-
--- Compare objects
-    open c_dev_objects;
-    open c_prod_objects;
-    loop
-        fetch c_dev_objects into current_object, object_type, ddl;
-        exit when c_dev_objects%notfound;
-
-        if not exists(select 1
-                      from all_objects
-                      where owner = prod_schema_name
-                        and object_name = current_object
-                        and object_type = object_type) then
-            result := result || current_object || ' (' || object_type || ')' || ' is not found in ' ||
-                      prod_schema_name ||
-                      ' schema' || chr(10);
-            continue;
-        end if;
-
-        execute immediate 'alter session set current_schema = ' || dev_schema_name;
-        execute immediate 'select dbms_metadata.compare_' || object_type || '_ddl(''' || current_object || ''', ''' ||
-                          dev_schema_name || ''', ''' || prod_schema_name || ''') from dual' into result;
-        if result is not null then
-            diff_list.extend;
-            diff_list(diff_list.count) := current_object || ' (' || object_type || ')' || ': ' || result;
-        end if;
-    end loop;
-
-    if diff_list.count > 0 then
-        result := result || 'Objects with differences:' || chr(10);
-        for i in 1..diff_list.count
-            loop
-                result := result || diff_list(i) || chr(10);
-            end loop;
-    end if;
-
-    close c_dev_objects;
-    close c_prod_objects;
-
--- Delete objects missing in prod
-    execute immediate 'alter session set current_schema = ' || prod_schema_name;
-    for dev_obj in (SELECT object_name, object_type
-                    FROM all_objects
-                    WHERE owner = dev_schema_name
-                      AND object_type IN ('FUNCTION', 'PROCEDURE', 'PACKAGE', 'PACKAGE BODY', 'INDEX')
-                      AND NOT EXISTS(SELECT 1
-                                     FROM all_objects
-                                     WHERE owner = prod_schema_name
-                                       AND object_name = dev_obj.object_name
-                                       AND object_type = dev_obj.object_type))
-        loop
-            execute immediate 'drop ' || dev_obj.object_type || ' ' || dev_obj.object_name;
-            result := result || dev_obj.object_name || ' (' || dev_obj.object_type || ')' || ' was deleted from ' ||
-                      prod_schema_name || ' schema' || chr(10);
-        end loop;
-
-    return result;
-
-END compare_schemas;
-BEGIN
-    dbms_output.put_line(compare_schemas('DEV', 'PROD'));
-END;
 ----dev-----
+CREATE USER dev IDENTIFIED BY admin;
+GRANT ALL PRIVILEGES TO dev;
 alter session set current_schema = DEV;
 
-DROP TABLE USERS;
-CREATE TABLE USERS
-(
-    ID   NUMBER PRIMARY KEY NOT NULL,
-    NAME VARCHAR2(100)      NOT NULL,
-    AGE  NUMBER
-);
-CREATE TABLE CLIENTS
-(
-    ID      NUMBER PRIMARY KEY NOT NULL,
-    USER_ID NUMBER             NOT NULL,
-    CONSTRAINT fk_user FOREIGN KEY (USER_ID) REFERENCES USERS (ID),
-    phone   VARCHAR2(10)
-);
 ----prod----
+CREATE USER prod IDENTIFIED BY admin;
+GRANT ALL PRIVILEGES TO prod;
 alter session set current_schema = PROD;
-CREATE TABLE USERS
+
+----tables----
+DROP TABLE DEV.TABLE1;
+CREATE TABLE DEV.TABLE1
 (
-    ID   NUMBER PRIMARY KEY NOT NULL,
-    NAME VARCHAR2(100)      NOT NULL,
-    AGE  NUMBER
+    id    NUMBER       not null,
+    aboba VARCHAR2(59) not null,
+    CONSTRAINT table1_pk PRIMARY KEY (id)
 );
-CREATE TABLE CLIENTS
+DROP TABLE DEV.TABLE2;
+CREATE TABLE DEV.TABLE2
 (
-    ID      NUMBER PRIMARY KEY NOT NULL,
-    USER_ID NUMBER             NOT NULL,
-    CONSTRAINT fk_user FOREIGN KEY (USER_ID) REFERENCES USERS (ID),
-    email   VARCHAR2(100)
+    id    NUMBER(10)   not null,
+    aboba VARCHAR2(59) not null,
+    CONSTRAINT table2_pk PRIMARY KEY (id)
 );
+
+CREATE TABLE PROD.TABLE1
+(
+    id     NUMBER       not null,
+    aboba  VARCHAR2(59) not null,
+    aboba2 VARCHAR2(59),
+    CONSTRAINT table1_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE PROD.TABLE3
+(
+    id    NUMBER       not null,
+    aboba VARCHAR2(59) not null,
+    CONSTRAINT table3_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE DEV.REF
+(
+    id    NUMBER       not null,
+    aboba VARCHAR2(59) not null,
+    CONSTRAINT three_pk PRIMARY KEY (id)
+);
+
+CREATE TABLE DEV.REF1
+(
+    id    NUMBER(10)   not null,
+    id_2  NUMBER(10)   not null,
+    aboba VARCHAR2(59) not null,
+    CONSTRAINT two_pk PRIMARY KEY (id),
+    CONSTRAINT two_fk FOREIGN KEY (id_2) REFERENCES DEV.REF (id)
+);
+
+CREATE TABLE DEV.REF2
+(
+    id    NUMBER(10)   not null,
+    id_2  NUMBER(10)   not null,
+    aboba VARCHAR2(59) not null,
+    CONSTRAINT one_pk PRIMARY KEY (id),
+    CONSTRAINT one_fk FOREIGN KEY (id_2) REFERENCES DEV.REF1 (id)
+);
+
+CREATE TABLE DEV.CYCLE
+(
+    id NUMBER(10) not null,
+
+    CONSTRAINT pk PRIMARY KEY (id),
+    CONSTRAINT fk FOREIGN KEY (id) REFERENCES DEV.CYCLE (id)
+);
+----procedures----
+CREATE OR REPLACE PROCEDURE DEV.proc1(a VARCHAR2)
+    IS
+BEGIN
+    DBMS_OUTPUT.PUT_LINE(a);
+END;
+
+CREATE OR REPLACE PROCEDURE PROD.proc1(a VARCHAR2)
+    IS
+BEGIN
+    DBMS_OUTPUT.PUT_LINE(a);
+END;
+
+
+CREATE OR REPLACE PROCEDURE DEV.proc2(a VARCHAR2)
+    IS
+BEGIN
+    DBMS_OUTPUT.PUT_LINE(a);
+END;
+
+CREATE OR REPLACE PROCEDURE PROD.proc2(a VARCHAR2)
+    IS
+BEGIN
+    DBMS_OUTPUT.PUT_LINE(a);
+    DBMS_OUTPUT.PUT_LINE(a);
+END;
+
+CREATE OR REPLACE PROCEDURE DEV.proc3(a VARCHAR2)
+    IS
+BEGIN
+    DBMS_OUTPUT.PUT_LINE(a);
+END;
+
+CREATE OR REPLACE PROCEDURE PROD.proc4(a VARCHAR2)
+    IS
+BEGIN
+    DBMS_OUTPUT.PUT_LINE(a);
+END;
+----functions----
+CREATE OR REPLACE Function dev.FU1(a in VARCHAR2)
+    return NUMBER
+    IS
+BEGIN
+    DBMS_OUTPUT.PUT_LINE(a);
+    RETURN 5;
+END;
+----indexes----
+CREATE INDEX dev.some_index
+    ON dev.TABLE1 (aboba);
